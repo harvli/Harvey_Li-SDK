671fab0c4ffb7eaba38f742aad2d889136182902
harvli <hl.138@hotmail.com>
2022-12-28 15:22:31 -0500

    SDK implementation

--- SDK-Test/dist/app.js
+++ SDK-Test/dist/app.js
@@ -1,16 +1,21 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const Harvey_SDK_1 = __importDefault(require("../../Harvey-SDK"));
+'use strict';
+var __importDefault =
+	(this && this.__importDefault) ||
+	function (mod) {
+		return mod && mod.__esModule ? mod : { default: mod };
+	};
+Object.defineProperty(exports, '__esModule', { value: true });
+const Harvey_SDK_1 = __importDefault(require('../../Harvey-SDK'));
 const client = new Harvey_SDK_1.default({});
 client.getAllBooks().then((books) => {
-    console.log(books);
+	console.log('Getting all books --------------------------------');
+	console.log(books);
 });
 client.getBookById('5cf5805fb53e011a64671582').then((book) => {
-    console.log(book);
+	console.log('Getting books with ID argument  --------------------------------');
+	console.log(book);
 });
 client.getAllChaptersOfBook('5cf5805fb53e011a64671582').then((chapters) => {
-    console.log(chapters);
+	console.log('Getting all chapters of book  --------------------------------');
+	console.log(chapters);
 });
